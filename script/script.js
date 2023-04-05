c77e0634840f5658579cc4681c6da95d9026b611
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-04-04 23:09:38 -0300

    creating html and css structure

---
+++ script/script.js
@@ -0,0 +1,4 @@
+/*api open weather pra testar eu uso o link com a key oferecida no menu "minhas keys" no site:
+link:https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=
+key:5455717c09524f86829bfe631bb63ec0                   */
+
